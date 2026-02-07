15e159c1fc86b19a79f7da5141947483a89186ed
Pragyan Pradipta Sahoo <pegyan2023@gmail.com>
2026-02-08 03:09:54 +0530

    Revert "Fix React Three Fiber canvas rendering"

--- frontend/src/Satellite.jsx
+++ frontend/src/Satellite.jsx
@@ -1,7 +1,6 @@
 
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import * as THREE from "three";
 
 export default function Satellite({ radius, speed, tilt, target }) {
   const ref = useRef();
