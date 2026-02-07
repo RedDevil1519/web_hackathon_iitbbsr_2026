9fb33a7a7d1bce3ae6854761a24d75e783e79ae4
Pragyan Pradipta Sahoo <pegyan2023@gmail.com>
2026-02-08 03:40:10 +0530

    Update Vite config with new base path.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist',
-  },
+  base: '/web_hackathon_iitbbsr_2026/'
 })
-npm run deploy
